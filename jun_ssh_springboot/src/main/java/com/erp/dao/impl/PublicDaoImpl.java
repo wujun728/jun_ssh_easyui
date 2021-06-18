730aa3af8a1a3c2d6c67f256bc4e47adc2709a9b
wujun728 <wujun728@hotmail.com>
2021-06-19 02:46:46 +0800

    1

--- jun_ssh_springboot/src/main/java/com/erp/dao/impl/PublicDaoImpl.java
+++ jun_ssh_springboot/src/main/java/com/erp/dao/impl/PublicDaoImpl.java
@@ -77,6 +77,7 @@ public class PublicDaoImpl<T>  implements PublicDao<T> {
 		Constants.getLogs(this.getCurrentSession(), o, Constants.LOGS_DELETE, Constants.LOGS_DELETE_TEXT, Constants.LOGS_DELETE_NAME);
 		tx.commit();
 	}
+	
 	@Transactional
 	public void saveOrUpdate(T o) {
 		Transaction tx = getCurrentSession().beginTransaction();
@@ -84,17 +85,20 @@ public class PublicDaoImpl<T>  implements PublicDao<T> {
 		tx.commit();
 	}
 
+	@Transactional(readOnly=true)
 	public List<T> find(String hql) {
-		return this.sessionFactory.openSession().createQuery(hql).list();
+		return this.sessionFactory.getCurrentSession().createQuery(hql).list();
 	}
 	
 	@SuppressWarnings("rawtypes")
+	@Transactional(readOnly=true)
 	public List findBySQL(String sql) {
-		return this.sessionFactory.openSession().createSQLQuery(sql).list();
+		return this.sessionFactory.getCurrentSession().createSQLQuery(sql).list();
 	}
 	
+	@Transactional(readOnly=true)
 	public List<T> find(String hql, Map<String, Object> params) {
-		Query q = this.sessionFactory.openSession().createQuery(hql);
+		Query q = this.sessionFactory.getCurrentSession().createQuery(hql);
 		if (params != null && !params.isEmpty()) {
 			for (String key : params.keySet()) {
 				q.setParameter(key, params.get(key));
@@ -103,6 +107,7 @@ public class PublicDaoImpl<T>  implements PublicDao<T> {
 		return q.list();
 	}
 	
+	@Transactional(readOnly=true)
 	public List<T> find(String hql, Map<String, Object> params, Integer page, Integer rows) {
 		if (page == null || page < 1) {
 			page = 1;
@@ -110,7 +115,7 @@ public class PublicDaoImpl<T>  implements PublicDao<T> {
 		if (rows == null || rows < 1) {
 			rows = 10;
 		}
-		Query q = this.sessionFactory.openSession().createQuery(hql);
+		Query q = this.sessionFactory.getCurrentSession().createQuery(hql);
 		if (params != null && !params.isEmpty()) {
 			for (String key : params.keySet()) {
 				q.setParameter(key, params.get(key));
@@ -120,10 +125,12 @@ public class PublicDaoImpl<T>  implements PublicDao<T> {
 	}
 
 
+	@Transactional(readOnly=true)
 	public T get(Class<T> c, Serializable id) {
-		return (T) this.sessionFactory.openSession().get(c, id);
+		return (T) this.sessionFactory.getCurrentSession().get(c, id);
 	}
 	
+	@Transactional(readOnly=true)
 	public T get(String hql, Map<String, Object>  param) {
 		List<T> l = this.find(hql, param);
 		if (l != null && l.size() > 0) {
@@ -133,12 +140,14 @@ public class PublicDaoImpl<T>  implements PublicDao<T> {
 		}
 	}
 
+	@Transactional(readOnly=true)
 	public Long count(String hql) {
-		return (Long) this.sessionFactory.openSession().createQuery(hql).uniqueResult();
+		return (Long) this.sessionFactory.getCurrentSession().createQuery(hql).uniqueResult();
 	}
-	
+
+	@Transactional(readOnly=true)
 	public Long count(String hql, Map<String, Object> params) {
-		Query q = this.sessionFactory.openSession().createQuery(hql);
+		Query q = this.sessionFactory.getCurrentSession().createQuery(hql);
 		if (params != null && !params.isEmpty()) {
 			for (String key : params.keySet()) {
 				q.setParameter(key, params.get(key));
@@ -148,12 +157,14 @@ public class PublicDaoImpl<T>  implements PublicDao<T> {
 	}
 	
 
+	@Transactional
 	public Integer executeHql(String hql) {
-		return this.sessionFactory.openSession().createQuery(hql).executeUpdate();
+		return this.sessionFactory.getCurrentSession().createQuery(hql).executeUpdate();
 	}
-	
+
+	@Transactional
 	public Integer executeHql(String hql, Map<String, Object> params) {
-		Query q = this.sessionFactory.openSession().createQuery(hql);
+		Query q = this.sessionFactory.getCurrentSession().createQuery(hql);
 		if (params != null && !params.isEmpty()) {
 			for (String key : params.keySet()) {
 				q.setParameter(key, params.get(key));
